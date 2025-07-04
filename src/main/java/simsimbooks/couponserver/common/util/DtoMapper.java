78fa582c31948c75de6ced96e98cd0abc4cc76d9
kodongwon <dw9706@gmail.com>
2025-07-05 04:59:17 +0900

    feat: Entity -> DTO 변환 Mapper 구현

---
+++ src/main/java/simsimbooks/couponserver/common/util/DtoMapper.java
@@ -0,0 +1,11 @@
+package simsimbooks.couponserver.common.util;
+
+public class DtoMapper {
+    public static <T, U> T toDto(U entity, Class<T> tClass) {
+        try {
+            return tClass.getConstructor(entity.getClass()).newInstance(entity);
+        } catch (Exception e) {
+            throw new RuntimeException("DTO 변환에 실패했습니다.");
+        }
+    }
+}
