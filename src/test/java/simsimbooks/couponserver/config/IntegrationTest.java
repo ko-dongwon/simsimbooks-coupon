54319e1ddc8d4440e65713e36736e897cc015f1b
kodongwon <dw9706@gmail.com>
2025-07-27 22:41:28 +0900

    feat: Test 코드 Redis 설정

--- src/test/java/simsimbooks/couponserver/config/IntegrationTest.java
+++ src/test/java/simsimbooks/couponserver/config/IntegrationTest.java
@@ -11,7 +11,7 @@ import java.lang.annotation.Target;
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
 @SpringBootTest
-@Import({TestDatabaseConfig.class})
+@Import({TestDatabaseConfig.class, TestRedisConfig.class})
 public @interface IntegrationTest{
 
 }
